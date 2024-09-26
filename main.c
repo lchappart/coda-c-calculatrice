39f1adb81904210940de0974fd2a6a9cf16555b7
lchappart <leo.chappart@coda-student.school>
2024-09-26 14:15:41 +0200

    etape 3 : recuperer les infos

--- main.c
+++ main.c
@@ -6,5 +6,11 @@ int main(int argc, char const *argv[])
 	int var1;
 	int var2;
 	char choix;
+	printf("Bonjour, choisissez un opérateur parmi: +, -, *, /, %%.\n");
+	scanf("%c", &choix);
+	printf("Choisissez maintenant un premier nombre");
+	scanf("%d", var1);
+	printf("Choisissez un second nombre.");
+	scanf("%d", var2);
 	return 0;
 }
