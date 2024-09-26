f65eb677433730d791e643b0d93214d330646865
lchappart <leo.chappart@coda-student.school>
2024-09-26 14:26:29 +0200

    etape 4 : création des conditions

--- main.c
+++ main.c
@@ -8,9 +8,35 @@ int main(int argc, char const *argv[])
 	char choix;
 	printf("Bonjour, choisissez un opérateur parmi: +, -, *, /, %%.\n");
 	scanf("%c", &choix);
-	printf("Choisissez maintenant un premier nombre");
-	scanf("%d", var1);
-	printf("Choisissez un second nombre.");
-	scanf("%d", var2);
+	printf("Choisissez maintenant un premier nombre\n");
+	scanf("%d", &var1);
+	printf("Choisissez un second nombre.\n");
+	scanf("%d", &var2);
+	if (choix == '+')
+	{
+		printf("Addition\n");
+	}
+	else if (choix == '-')
+	{
+		printf("Soustraction\n");
+	}
+	else if (choix == '*')
+	{
+		printf("Multiplication\n");
+	}
+	else if (choix == '/')
+	{
+		printf("Division\n");
+	}
+	else if (choix == '%')
+	{
+		printf("Modulo\n");
+	}
+	else
+	{
+		printf("L'operateur n'est pas reconnu.\n");
+	}
+
+
 	return 0;
 }
