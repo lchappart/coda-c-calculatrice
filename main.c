85a905d252b7301c0bc1c2f786665575d9224ee2
lchappart <leo.chappart@coda-student.school>
2024-09-26 14:33:41 +0200

    etape 5 : l'opération

--- main.c
+++ main.c
@@ -3,34 +3,56 @@
 
 int main(int argc, char const *argv[])
 {
-	int var1;
-	int var2;
+	double var1;
+	double var2;
+	double resultat;
 	char choix;
 	printf("Bonjour, choisissez un opérateur parmi: +, -, *, /, %%.\n");
 	scanf("%c", &choix);
 	printf("Choisissez maintenant un premier nombre\n");
-	scanf("%d", &var1);
+	scanf("%.2f", &var1);
 	printf("Choisissez un second nombre.\n");
-	scanf("%d", &var2);
+	scanf("%.2f", &var2);
 	if (choix == '+')
 	{
 		printf("Addition\n");
+		resultat = var1 + var2;
 	}
 	else if (choix == '-')
 	{
 		printf("Soustraction\n");
+		resultat = var1 - var2;
 	}
 	else if (choix == '*')
 	{
 		printf("Multiplication\n");
+		resultat = var1 * var2;
 	}
 	else if (choix == '/')
 	{
 		printf("Division\n");
+		if (var2 == 0)
+		{
+			resultat = var1 / var2;
+		}
+		else
+		{
+			printf("On ne peut pas diviser par 0.\n")
+		}
+		
 	}
 	else if (choix == '%')
 	{
 		printf("Modulo\n");
+		if (var2 == 0)
+		{
+			resultat = var1 % var2;
+		}
+		else
+		{
+			printf("On ne peut pas diviser par 0.\n")
+		}
+
 	}
 	else
 	{
